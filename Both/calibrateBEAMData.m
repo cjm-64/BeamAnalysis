826873b62ad3363e36fc092c41f7c874056123ab
Chris <61848401+cjm-64@users.noreply.github.com>
2024-04-20 17:27:42 -0400

    Working on calibration application

--- Both/calibrateBEAMData.m
+++ Both/calibrateBEAMData.m
@@ -2,11 +2,16 @@ function testDataCalibrated = calibrateBEAMData(testDataCentered, calibrationCoe
     
     names = fieldnames(testDataCentered);
     for name = 1:numel(names)
+        disp(name)
         if names(name) == "time"
             testDataCalibrated.time = testDataCentered.time;
+            continue;
         else
             directions = fieldnames(testDataCentered.(names{name}));
             for dir = 1:numel(directions)
+                disp(dir)
+                part1 = testDataCentered.(names{name}).(directions{dir});
+                part2 = calibrationCoeffs.(names{name});
                 testDataCalibrated.(names{name}).(directions{dir}) = testDataCentered.(names{name}).(directions{dir})./calibrationCoeffs.(names{name});
             end
         end
